84f8fec164771c6962d734ddaee827f08c12de40
phamhiep2506 <phamhiep25062k@gmail.com>
2024-06-04 10:52:24 +0700

    format code hook.js

--- hook.js
+++ hook.js
@@ -1,35 +1,49 @@
-Java.perform(function() {
+Java.perform(function () {
+  console.log("[INFO] Starting hook com.wmspanel.larix_screencaster");
 
-  console.log("[+] Starting hook com.wmspanel.larix_screencaster");
-
-  Java.use("com.wmspanel.screencast.MainActivity").isSubscribed.overload('com.wmspanel.screencast.billing.PremiumFeature').implementation  = function(arg1) {
-    console.log("[+] Hook MainActivity isSubscribed()");
-    console.log("Params: " + arg1);
+  Java.use("com.wmspanel.screencast.MainActivity").isSubscribed.overload(
+    "com.wmspanel.screencast.billing.PremiumFeature",
+  ).implementation = function (arg) {
+    console.log("[INFO] Hook MainActivity.isSubscribed()");
+    console.log("[LOG] " + arg);
     return true;
-  }
+  };
 
-  Java.use("com.wmspanel.screencast.StreamerService").isSubscribed.overload('com.wmspanel.screencast.billing.PremiumFeature').implementation  = function(arg1) {
-    console.log("[+] Hook StreamerService isSubscribed()");
-    console.log("Params: " + arg1);
+  Java.use("com.wmspanel.screencast.StreamerService").isSubscribed.overload(
+    "com.wmspanel.screencast.billing.PremiumFeature",
+  ).implementation = function (arg) {
+    console.log("[INFO] Hook StreamerService.isSubscribed()");
+    console.log("[LOG] " + arg);
     return true;
-  }
+  };
 
-  Java.use("com.wmspanel.screencast.preference.PreferenceFragmentBase").isSubscribed.overload('com.wmspanel.screencast.billing.PremiumFeature').implementation  = function(arg1) {
-    console.log("[+] Hook PreferenceFragmentBase isSubscribed()");
-    console.log("Params: " + arg1);
+  Java.use(
+    "com.wmspanel.screencast.preference.PreferenceFragmentBase",
+  ).isSubscribed.overload(
+    "com.wmspanel.screencast.billing.PremiumFeature",
+  ).implementation = function (arg) {
+    console.log("[INFO] Hook PreferenceFragmentBase.isSubscribed()");
+    console.log("[LOG] " + arg);
     return true;
-  }
+  };
 
-  Java.use("com.wmspanel.screencast.preference.SettingsActivity").isSubscribed.overload('com.wmspanel.screencast.billing.PremiumFeature').implementation  = function(arg1) {
-    console.log("[+] Hook SettingsActivity PremiumFeature isSubscribed()");
-    console.log("Params: " + arg1);
+  Java.use(
+    "com.wmspanel.screencast.preference.SettingsActivity",
+  ).isSubscribed.overload(
+    "com.wmspanel.screencast.billing.PremiumFeature",
+  ).implementation = function (arg) {
+    console.log("[INFO] Hook SettingsActivity.PremiumFeature.isSubscribed()");
+    console.log("[LOG] " + arg);
     return true;
-  }
+  };
 
-  Java.use("com.wmspanel.screencast.preference.SettingsActivity").isSubscribed.overload('com.wmspanel.screencast.billing.SubscriptionPlan').implementation  = function(arg1) {
-    console.log("[+] Hook SettingsActivity SubscriptionPlan isSubscribed()");
-    console.log("Params: " + arg1);
+  Java.use(
+    "com.wmspanel.screencast.preference.SettingsActivity",
+  ).isSubscribed.overload(
+    "com.wmspanel.screencast.billing.SubscriptionPlan",
+  ).implementation = function (arg) {
+    console.log("[INFO] Hook SettingsActivity.SubscriptionPlan.isSubscribed()");
+    console.log("[LOG] " + arg);
     return true;
-  }
-
+  };
 });
